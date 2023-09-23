8f14de39c7c07919a45c9d22b299b00b16f88a7c
Murilo Junior <89697677+murjunior@users.noreply.github.com>
2023-09-23 12:18:45 -0300

    Add files via upload

---
+++ redirecionador-url.js
@@ -0,0 +1,20 @@
+// Desenvolvido por Elevafoco
+// Site: www.elevafoco.com.br
+// Versão: 0.1
+function hasClass(el, clss) {
+    return el.className && new RegExp("(^|\\s)" +
+        clss + "(\\s|$)").test(el.className);
+}
+window.onload = function() {
+    var veirifi = document.querySelector(".veirificaclasse");
+    if (hasClass(veirifi, "redirecionou")) {
+        var urlRedirect = document.getElementsByTagName("a")[0].href;
+        setTimeout(function() {
+            window.location.href = urlRedirect;
+            console.log(urlRedirect);
+        }, 1300);
+        console.log('Tem o HTML, estamos redirecionando!')
+    } else {
+        console.log('Não encontrei o HTML necessário.')
+    }
+}
